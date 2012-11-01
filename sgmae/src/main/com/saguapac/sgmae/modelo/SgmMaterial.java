24e61fae1e00dc08d34d0b99062767a12a70307b
oar0981 <oar0981@ggoar0981.saguapac.com.bo>
2012-11-01 10:37:02 -0400

    jueves 11

--- sgmae/src/main/com/saguapac/sgmae/modelo/SgmMaterial.java
+++ sgmae/src/main/com/saguapac/sgmae/modelo/SgmMaterial.java
@@ -1,18 +1,30 @@
 package com.saguapac.sgmae.modelo;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="SGM_MATERIAL")
-public class SgmMaterial {
+public class SgmMaterial implements Serializable {
+
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@Column(name="ID_MATERIAL", nullable=false)
@@ -37,10 +49,132 @@ public class SgmMaterial {
 	@Column(name="EXISTENCIA_MINIMA", precision=2)
 	private BigDecimal existenciaMinima;
 	
-	
-
 	@Embedded 
 	private Auditorias auditorias;
+
+	@OneToMany(fetch=FetchType.LAZY, mappedBy="sgmMaterial")
+	private List<SgmOtMaterial> sgmOtMaterial;
+
+	@ManyToOne(optional=false,fetch=FetchType.LAZY)
+	private SgmCategoriaMaterial sgmCategoriaMaterial;
+
+	public Long getIdMaterial() {
+		return idMaterial;
+	}
+
+
+
+	public void setIdMaterial(Long idMaterial) {
+		this.idMaterial = idMaterial;
+	}
+
+
+
+	public String getCodigoMaterial() {
+		return codigoMaterial;
+	}
+
+
+
+	public void setCodigoMaterial(String codigoMaterial) {
+		this.codigoMaterial = codigoMaterial;
+	}
+
+
+
+	public String getNombreMaterial() {
+		return nombreMaterial;
+	}
+
+
+
+	public void setNombreMaterial(String nombreMaterial) {
+		this.nombreMaterial = nombreMaterial;
+	}
+
+
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+
+
+	public BigDecimal getCostoUnitario() {
+		return costoUnitario;
+	}
+
+
+
+	public void setCostoUnitario(BigDecimal costoUnitario) {
+		this.costoUnitario = costoUnitario;
+	}
+
+
+
+	public BigDecimal getExistencia() {
+		return existencia;
+	}
+
+
+
+	public void setExistencia(BigDecimal existencia) {
+		this.existencia = existencia;
+	}
+
+
+
+	public BigDecimal getExistenciaMinima() {
+		return existenciaMinima;
+	}
+
+
+
+	public void setExistenciaMinima(BigDecimal existenciaMinima) {
+		this.existenciaMinima = existenciaMinima;
+	}
+
+
+
+	public Auditorias getAuditorias() {
+		return auditorias;
+	}
+
+
+
+	public void setAuditorias(Auditorias auditorias) {
+		this.auditorias = auditorias;
+	}
+
+
+
+	public void setSgmOtMaterial(List<SgmOtMaterial> sgmOtMaterial) {
+		this.sgmOtMaterial = sgmOtMaterial;
+	}
+
+
+
+	public List<SgmOtMaterial> getSgmOtMaterial() {
+		return sgmOtMaterial;
+	}
+
+
+
+	public void setSgmCategoriaMaterial(SgmCategoriaMaterial sgmCategoriaMaterial) {
+		this.sgmCategoriaMaterial = sgmCategoriaMaterial;
+	}
+
+
+
+	public SgmCategoriaMaterial getSgmCategoriaMaterial() {
+		return sgmCategoriaMaterial;
+	}
 	
 	
 	
--- sgmae/src/main/com/saguapac/sgmae/modelo/SgmOrdenTrabajo.java
+++ sgmae/src/main/com/saguapac/sgmae/modelo/SgmOrdenTrabajo.java
@@ -2,21 +2,24 @@ package com.saguapac.sgmae.modelo;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="SgmOrdenTrabajo")
+@Table(name="SGM_ORDEN_TRABAJO")
 public class SgmOrdenTrabajo implements Serializable {
 
 	
@@ -38,13 +41,18 @@ public class SgmOrdenTrabajo implements Serializable {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date fechaFinOT;
 	
+	
 	@Embedded 
 	private Auditorias auditorias;
 	
 	@Enumerated(EnumType.STRING)
 	private TipoOrdenTrabajo tipo;
 
-
+    @OneToMany(fetch=FetchType.LAZY, mappedBy="SgmOrdenTrabajo")
+	private List<SgmOtMaterial> SgmOtMaterial;
+	
+    @OneToMany(fetch=FetchType.LAZY, mappedBy="sgmOrdenTrabajo")
+    private List<SgmOtTiposTrabajo> sgmOtTiposTrabajo;
 	
 	public SgmOrdenTrabajo() {
 		
@@ -97,6 +105,23 @@ public class SgmOrdenTrabajo implements Serializable {
 	public TipoOrdenTrabajo getTipo() {
 		return tipo;
 	}
-	
+
+	public void setSgmOtMaterial(List<SgmOtMaterial> sgmOtMaterial) {
+		SgmOtMaterial = sgmOtMaterial;
+	}
+
+	public List<SgmOtMaterial> getSgmOtMaterial() {
+		return SgmOtMaterial;
+	}
+
+	public void setSgmOtTiposTrabajo(List<SgmOtTiposTrabajo> sgmOtTiposTrabajo) {
+		this.sgmOtTiposTrabajo = sgmOtTiposTrabajo;
+	}
+
+	public List<SgmOtTiposTrabajo> getSgmOtTiposTrabajo() {
+		return sgmOtTiposTrabajo;
+	}
+
+
 	
 }
